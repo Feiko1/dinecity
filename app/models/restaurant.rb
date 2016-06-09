b59f0473e36dabb89c810affaedd882f3dd3a1e0
ryan jeff <lrjbrual@live.com>
2016-06-09 16:49:58 -0700

    Adding layouts design  and Search functionality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,8 @@ class Restaurant < ActiveRecord::Base
     has_many :photos, dependent: :destroy
     has_many :deals, dependent: :destroy
 
+    def self.search(search)
+      where("name LIKE ?", "%#{search}%")
+      where("city_name LIKE ?", "%#{search}%")
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
 end
