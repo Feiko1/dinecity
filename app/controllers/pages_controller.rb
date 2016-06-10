da89970febc48cfc31f2b5280d44077bea08945f
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-10 17:56:22 +0200

    heroku bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   #test branchtest
   #
-  skip_before_action :authenticate_user!, only: :home, :index, :show #:booking
+  skip_before_action :authenticate_user!, only: :home, :index, :show
 
 
   def home
