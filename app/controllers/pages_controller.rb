6de74dfccce0b0e5d5f38faea0725b440d36d559
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-10 18:00:18 +0200

    yet another bad form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   #test branchtest
   #
-  skip_before_action :authenticate_user!, only: :home, :index, :show
+  skip_before_action :authenticate_user!, only: :home  #, :index, :show
 
 
   def home
