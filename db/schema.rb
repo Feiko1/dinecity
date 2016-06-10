00f242459d54b9debe88e1e80ba31f4c360800a7
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-10 17:07:07 +0200

    changed button link

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160610095245) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,12 +122,12 @@ ActiveRecord::Schema.define(version: 20160610095245) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "kind"
-    t.boolean  "admin",                  default: false, null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "picture"
     t.string   "token"
     t.datetime "token_expiry"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
