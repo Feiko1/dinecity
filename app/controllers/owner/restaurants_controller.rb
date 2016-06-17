3562cb881ea1323071ba4b0f0a1e7599a0edaaf4
lrjbrual <lrjbrual@live.com>
2016-06-17 10:28:40 -0700

    Adding Helper and fixing the controller layouts

--- app/controllers/owner/restaurants_controller.rb
+++ app/controllers/owner/restaurants_controller.rb
@@ -1,19 +1,33 @@
 module Owner
-  class RestaurantsController < ApplicationController
-    layout "owner"
-
-#    before_action :requires_owner
-
+  class RestaurantsController < Owner::BaseController
     def index
+      @restaurants = current_user.restaurants
+    end
+
+    def edit
+      @restaurant = current_user.restaurants.find(params[:id])
+    end
+
+    def update
+      @restaurant = current_user.restaurants.find(params[:id])
+      @restaurant.update(restaurant_params)
+
+      redirect_to edit_owner_restaurant_path(@restaurant)
     end
 
     private
 
-    # def requires_owner
-    #   unless current_user.kind == "owner" # REMEMBER TO CHANGE BACK TO 'owner'
-    #     flash[:alert] = "You are not owner of any restaurant"
-    #     redirect_to root_path
-    #   end
-    # end
+    def restaurant_params
+      params.require(:restaurant).permit(
+        :name,
+        :email_address,
+        :phone_number,
+        :street_number,
+        :street_name,
+        :city_name,
+        :country_name,
+        :postal_code
+      )
+    end
   end
 end
