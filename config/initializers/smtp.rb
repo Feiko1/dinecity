9e406e486add6c1ee388a68cdc2dc6d29e559ef7
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-20 18:20:59 +0200

    add PostMark API authorization

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.postmarkapp.com',
   port: '25',
   domain: 'heroku.com',
-  user_name: ENV['POSTMARK_API_TOKEN'],
-  password: ENV['POSTMARK_API_TOKEN'],
+  user_name: ENV['eb933cbd-11ed-47c5-8cd5-0a4764a773b3'],
+  password: ENV['eb933cbd-11ed-47c5-8cd5-0a4764a773b3'], #CHECK this
   authentication: :cram_md5,
   enable_starttls_auto: true
 }
