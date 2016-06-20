49289a0b3c688d5f1390f0ff18bf367659fa145a
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-20 14:48:25 +0200

    add operational mailers for user registration, restaurant creation, booking creation

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -5,9 +5,37 @@ class BookingMailer < ApplicationMailer
   #
   #   en.booking_mailer.user_booking_confirmation.subject
   #
-  def user_booking_confirmation
-    # @greeting = "Hi"
+  def user_booking_confirmation(booking) #, user, restaurant
+    @booking = booking
 
-    mail to: "to@example.org"
+
+    if booking.user
+      @user = booking.user
+      @email = @user.email
+      @first_name =@user.first_name
+      @last_name = @user.last_name
+    else
+      @email = booking.visitor_email
+      @first_name = booking.visitor_first_name
+      @last_name = booking.visitor_last_name
+    end
+
+    @restaurant = booking.deal.restaurant
+
+    mail(
+      to:       @email,  #@user.email, ##CHECK THIS
+      subject:  "Please confirm your booking for a great evening at #{@restaurant.name}"
+      )
   end
+
+  # def owner_booking_request(booking, user, restaurant)
+  #   @booking = booking
+  #   @user = user
+  #   @restaurant = restaurant
+
+  #   mail(
+  #     to:       @restaurant.user.email, ##CHECK THIS
+  #     subject:  "booking request on #{@booking.date} by #{@owner.first_name} #{@owner.last_name}"
+  #     )
+  # end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,12 +8,19 @@ class Restaurant < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode #, if: :street_name_changed?
 
+  after_create :send_restaurant_creation_email  #
+
   def self.search(search)
-    where("name LIKE ?", "%#{search}%")
+    where("name LIKE ?", "%#{search}%") #TIS NEEDS TO BE MADE CASE INSENSITIVE
     where("city_name LIKE ?", "%#{search}%")
   end
 private
   def full_address
   self.street_name + self.street_number.to_s + self.city_name + self.country_name + self.postal_code
   end
+
+  def send_restaurant_creation_email
+    RestaurantMailer.creation_confirmation(self).deliver_now
+  end
+
 end
