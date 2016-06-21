c155eea980f815f6d33b9abd530a48ecbfe5bbed
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-21 19:31:25 +0200

    add mail logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } #used for facebook login API
 
-  resources :restaurants, only: [:index, :show] do  #basic filtering has distinct form on index page; this search box passes
+  resources :restaurants, only: [:index, :show, :create, :new] do  #basic filtering has distinct form on index page; this search box passes
     resources :reviews, only: :create
 
     resources :bookings, only: [:new, :create, :update, :show] do
