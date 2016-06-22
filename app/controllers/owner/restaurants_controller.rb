819a26ee63e0908a7c2dc803ab4464d798f3e324
lrjbrual <lrjbrual@live.com>
2016-06-22 15:39:26 -0700

    Adding functionality for offers

--- app/controllers/owner/restaurants_controller.rb
+++ app/controllers/owner/restaurants_controller.rb
@@ -11,33 +11,26 @@ module Owner
     def update
       @restaurant = current_user.restaurants.find(params[:id])
       @restaurant.update(restaurant_params)
+      redirect_to edit_owner_restaurant_path(current_user, @restaurant)
+    end
 
-      def index
-        redirect_to edit_owner_restaurant_path(@restaurant)
-      end
+      # def index
+      #   redirect_to edit_owner_restaurant_path(@restaurant)
+      # end
 
       private
 
-
-    # def requires_owner
-    #   unless current_user.kind == "owner"    # REMEMBER TO CHANGE BACK TO 'owner'
-    #     flash[:alert] = "You are not owner of any restaurant"
-    #     redirect_to root_path
-    #   end
-    # end
-
-    def restaurant_params
-      params.require(:restaurant).permit(
-          :name,
-          :email_address,
-          :phone_number,
-          :street_number,
-          :street_name,
-          :city_name,
-          :country_name,
-          :postal_code
-          )
-    end
+      def restaurant_params
+        params.require(:restaurant).permit(
+            :name,
+            :email_address,
+            :phone_number,
+            :street_number,
+            :street_name,
+            :city_name,
+            :country_name,
+            :postal_code
+            )
+      end
   end
-end
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,5 +2,4 @@ class Deal < ActiveRecord::Base
   belongs_to :restaurant
   has_many :bookings, dependent: :destroy
   has_many :courses, dependent: :destroy
-
 end
