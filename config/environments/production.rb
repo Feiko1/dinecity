f9455df411e8cd517e9e208b81e6ae679f65a9c5
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-23 16:11:22 +0200

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  config.action_mailer.default_url_options = { :host => 'HOST'}
+
 end
