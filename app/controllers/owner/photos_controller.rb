55b58e0924b1d4954eea1911b9cd01d6ea6715d5
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-27 17:35:21 +0200

    add functional front-end photos

--- app/controllers/owner/photos_controller.rb
+++ app/controllers/owner/photos_controller.rb
@@ -5,7 +5,7 @@ module Owner
 
     def create
       @photo = Photo.new(photo_params)
-      @restaurant = Restaurant.find(params[:restaurant_id])
+      @restaurant = current_user.restaurants.find(params[:restaurant_id])
 
       if @photo.save!
         flash[:success] = "Photo successfully uploaded"
@@ -16,8 +16,10 @@ module Owner
     end
 
     def destroy
-      @photo = Photo.find(params[:id])
+      @restaurant = current_user.restaurants.find(params[:restaurant_id])
+      @photo = @restaurant.photos.find(params[:id])
       @photo.destroy
+
       flash[:danger] = "Photo was successfully deleted"
       redirect_to edit_owner_restaurant_path(@restaurant)
     end
