029aeeaf915d53f21a2da29f1cdaae1dcbad7f15
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-28 11:40:25 +0200

    add half of meta-tags functionality

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Initialize default meta tags.
+DEFAULT_META = YAML.load_file(Rails.root.join('config/meta.yml'))
+
 # Initialize the Rails application.
 Rails.application.initialize!
