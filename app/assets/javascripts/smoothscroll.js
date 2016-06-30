ff725812a95c95d59719ab968e1fb37693703dbe
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-30 16:47:19 +0200

    fix JS hyperlink error

--- app/assets/javascripts/smoothscroll.js
+++ app/assets/javascripts/smoothscroll.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#123"]:not([href="#123"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
